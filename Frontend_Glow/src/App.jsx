1adbed517c963dccf3f2569677a9f9cea61c22bf
Hosanna Martin <vivahosanna@icloud.com>
2024-02-02 04:16:59 +0800

    scaffolding for fe and be

---
+++ Frontend_Glow/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+      <PrimeReactProvider>
+          
+      </PrimeReactProvider>
+  );
+}
+
+export default App
